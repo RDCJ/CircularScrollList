c1692776117859cac662168b082eb2c7366222bd
RDCJ <1175024349@qq.com>
2024-05-29 14:37:21 +0800

    Update CircularScrollList.cs

--- Assets/CircularScrollList/Scripts/CircularScrollList.cs
+++ Assets/CircularScrollList/Scripts/CircularScrollList.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 namespace SCL
 {
