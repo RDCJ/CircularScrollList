3d223b87717ee21e2b49e29595542e97194201bd
RDCJ <1175024349@qq.com>
2024-08-15 18:01:40 +0800

    Update PageSelectionViewExample.cs

--- Assets/PageSelectionView/Example/PageSelectionViewExample.cs
+++ Assets/PageSelectionView/Example/PageSelectionViewExample.cs
@@ -9,6 +9,7 @@ public class PageSelectionViewExample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+#if UNITY_EDITOR
         for (int i=0; i<4; i++)
         {
             GameObject page_prefab = AssetDatabase.LoadAssetAtPath<GameObject>($"Assets/PageSelectionView/Example/Page{i + 1}.prefab");
@@ -18,6 +19,7 @@ public class PageSelectionViewExample : MonoBehaviour
             page.transform.localScale = Vector3.one;
             page.transform.localPosition = Vector3.zero;
         }
+#endif
     }
 
     // Update is called once per frame
