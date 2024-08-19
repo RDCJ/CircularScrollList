df31a7827e52e2a7bb4f230158ddddafe4b39fe6
RDCJ <1175024349@qq.com>
2024-08-19 09:53:57 +0800

    modify

--- Assets/GestureEvent/Example/AddRectBoxCollider.cs
+++ Assets/GestureEvent/Example/AddRectBoxCollider.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class AddRectBoxCollider : MonoBehaviour
@@ -8,7 +5,7 @@ public class AddRectBoxCollider : MonoBehaviour
     private void Awake()
     {
         var rtf = GetComponent<RectTransform>();
-        var box_collider = this.AddComponent<BoxCollider2D>();
+        var box_collider = gameObject.AddComponent<BoxCollider2D>();
         box_collider.size = rtf.rect.size;
         box_collider.offset = -(rtf.pivot - new Vector2(0.5f, 0.5f)) * rtf.rect.size;
         Destroy(this);
