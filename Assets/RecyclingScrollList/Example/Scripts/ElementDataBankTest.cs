8e72e8632c9f0eb883536d8adf7b8c52e6a100a5
RDCJ <1175024349@qq.com>
2024-12-22 20:41:08 +0800

    添加命名空间

--- Assets/RecyclingScrollList/Example/Scripts/ElementDataBankTest.cs
+++ Assets/RecyclingScrollList/Example/Scripts/ElementDataBankTest.cs
@@ -1,61 +1,63 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using RSL;
 
-[Serializable]
-public class ElementDataTest
+namespace RSL.Test
 {
-    public string click_log;
-
-    public ElementDataTest(string click_log)
+    [Serializable]
+    public class ElementDataTest
     {
-        this.click_log = click_log;
+        public string click_log;
+
+        public ElementDataTest(string click_log)
+        {
+            this.click_log = click_log;
+        }
+    }
+
+    public class ElementDataBankTest : ElementDataBankBase<ElementDataTest>
+    {
+        public float random_noise_scale;
+        public float position_offset_scale;
+        public float sin_offset_frequency;
+        public List<ElementDataTest> data;
+        private List<float> randomFloat;
+
+        public override int ElementCount => data.Count;
+
+        public ElementDataBankTest()
+        {
+            data = new List<ElementDataTest>();
+            randomFloat = new List<float>();
+            for (int i = 0; i < 5000; i++)
+            {
+                data.Add(new ElementDataTest($"click: {i}"));
+                randomFloat.Add(UnityEngine.Random.Range(-1.0f, 1.0f) * random_noise_scale);
+            }
+        }
+
+        public override ElementDataTest GetElementData(int element_idx)
+        {
+            if (data != null && element_idx < data.Count && element_idx >= 0)
+            {
+                return data[element_idx];
+            }
+            return null;
+        }
+
+        public override Vector3 CalcElementPosition(RecyclingScrollList.ScrollType scrollType, int element_idx, Vector3 defaultPosition)
+        {
+            float sin_offset = MathF.Sin(element_idx * sin_offset_frequency);
+            float random_offset = randomFloat[element_idx];
+            if (scrollType == RecyclingScrollList.ScrollType.Vertical)
+            {
+                return defaultPosition + new Vector3(sin_offset + random_offset, 0, 0) * position_offset_scale;
+            }
+            else
+            {
+                return defaultPosition + new Vector3(0, sin_offset + random_offset, 0) * position_offset_scale;
+            }
+        }
     }
 }
 
-public class ElementDataBankTest : ElementDataBankBase<ElementDataTest>
-{
-    public float random_noise_scale;
-    public float position_offset_scale;
-    public float sin_offset_frequency;
-    public List<ElementDataTest> data;
-    private List<float> randomFloat;
-
-    public override int ElementCount => data.Count;
-
-    public ElementDataBankTest()
-    {
-        data = new List<ElementDataTest>();
-        randomFloat = new List<float>();
-        for (int i = 0; i < 5000; i++)
-        {
-            data.Add(new ElementDataTest($"click: {i}"));
-            randomFloat.Add(UnityEngine.Random.Range(-1.0f, 1.0f) * random_noise_scale);
-        }
-    }
-
-    public override ElementDataTest GetElementData(int element_idx)
-    {
-        if (data != null && element_idx < data.Count && element_idx >= 0)
-        {
-            return data[element_idx];
-        }
-        return null;
-    }
-
-    public override Vector3 CalcElementPosition(RecyclingScrollList.ScrollType scrollType, int element_idx, Vector3 defaultPosition)
-    {
-        float sin_offset = MathF.Sin(element_idx * sin_offset_frequency);
-        float random_offset = randomFloat[element_idx];
-        if (scrollType == RecyclingScrollList.ScrollType.Vertical)
-        {
-            return defaultPosition + new Vector3(sin_offset + random_offset, 0, 0) * position_offset_scale;
-        }
-        else
-        {
-            return defaultPosition + new Vector3(0, sin_offset + random_offset, 0) * position_offset_scale;
-        }
-    }
-}
--- Assets/RecyclingScrollList/Example/Scripts/RSLExample.cs
+++ Assets/RecyclingScrollList/Example/Scripts/RSLExample.cs
@@ -1,19 +1,17 @@
 using RSL;
 using UnityEngine;
 
-public class RSLExample : MonoBehaviour
+namespace RSL.Test
 {
-    private RecyclingScrollList circularScrollList;
-    // Start is called before the first frame update
-    void Start()
+    public class RSLExample : MonoBehaviour
     {
-        circularScrollList = this.GetComponent<RecyclingScrollList>();
-        circularScrollList.Init(new ElementDataBankTest());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private RecyclingScrollList circularScrollList;
+        // Start is called before the first frame update
+        void Start()
+        {
+            circularScrollList = this.GetComponent<RecyclingScrollList>();
+            circularScrollList.Init(new ElementDataBankTest());
+        }
     }
 }
+
